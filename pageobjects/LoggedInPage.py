575bd68dad195e4ed2c33bf7bf6223ad476c5743
jjd77 <jarekdys@gmail.com>
2022-05-11 17:01:54 +0200

    added logout

--- pageobjects/LoggedInPage.py
+++ pageobjects/LoggedInPage.py
@@ -12,6 +12,8 @@ class LoggedInPage:
         self.logger = logging.getLogger(__name__)
         self.my_account_page_title = "My Account"
         self.user_name_header_css = "h3.RTL"
+        self.user_menu_field_xpath = "//nav//div[2]/ul[2]/ul/li[1]/a"
+        self.user_menu_logout_field_xpath = "//nav//a[text()='  Logout']"
 
 
     @allure.step("veryfy page title")
@@ -28,4 +30,12 @@ class LoggedInPage:
         self.logger.info(expected_header)
         assert header_text_string == expected_header
         self.logger.info("User header on the user profile got the name: " + header_text.text)
+
+    @allure.step("user logout")
+    def logout_user(self):
+        self.driver.find_element_by_xpath(self.user_menu_field_xpath).click()
+        self.driver.find_element_by_xpath(self.user_menu_logout_field_xpath).click()
+        assert self.driver.title != self.my_account_page_title
+
+
 
--- tests/test_vacations_desktop.py
+++ tests/test_vacations_desktop.py
@@ -93,5 +93,6 @@ class TestCases(Setup):
         my_account_page = LoggedInPage(self.driver)
         my_account_page.check_user_header(username, user_surname)
         my_account_page.verify_my_account_page_title()
+        my_account_page.logout_user()
 
 
