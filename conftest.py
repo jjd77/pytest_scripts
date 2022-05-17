8f9fd3ced26aadfdcad905b0550b610a872da20b
jjd77 <99016778+jjd77@users.noreply.github.com>
2022-05-17 15:43:00 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,3 +1,3 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) #this fixes the issue I had with Python bug (importing files from higher directories)
